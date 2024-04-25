ea3d7951825eee253c6f8107684e85519b9d127a
Yeferson69 <113728587+Yeferson69@users.noreply.github.com>
2024-04-24 22:24:39 -0500

    Update conection.php

--- conection.php
+++ conection.php
@@ -1,8 +1,6 @@
 <?php
-
 // Conexión a la base de datos SQL Server
-class Db 
-    {
+class Db {
     private $host      = 'smc24.database.windows.net';
     private $user      = 'SMc2024';
     private $dbname    = 'Votos belleza marina 2024';
@@ -11,74 +9,66 @@ class Db
     private static $instance;
     private $dbh;
 
-    public function __construct()
-    {
+    public function __construct(){
         // DSN: cadena de conexión
         $dsn = 'sqlsrv:server=' . $this->host . ';Database=' . $this->dbname;
 
         try{
             $this->dbh = new PDO($dsn, $this->user, $this->pass);
-        }
+    }
         // Captura los errores
         catch(PDOException $e){
             $this->error = $e->getMessage();
             throw new Exception("Error de conexión a la base de datos: " . $this->error);
-        }
     }
+}
 
-
-
-    public static function getInstance()
-    {
-        if (self::$instance == null) 
-        {
-            $className = __CLASS__;
-            self::$instance = new $className();
-        }
-        return self::$instance;
+public static function getInstance(){
+    if (self::$instance == null){
+        $className = __CLASS__;
+        self::$instance = new $className();
     }
+    return self::$instance;
+}
 
-    // Preparar la sentencia SQL para insertar datos en la tabla usuarios
+// Preparar la sentencia SQL para insertar datos en la tabla usuarios
     
+public function bind(){
+    $nameCandidata = $_POST['nameSeleccionada'];
+    $regionCandidata = $_POST['descripcionSeleccionada'];
+    $categoriaCandidata = $_POST['categoria'];
+    $nombreVotante = $_POST['user_name'];        
+    $celularVotante = $_POST['user_phone'];
+    $correoVotante = $_POST['user_correo'];
 
-    public function bind()
-    {
-        $nameCandidata = $_POST['nameSeleccionada'];
-        $regionCandidata = $_POST['descripcionSeleccionada'];
-        $categoriaCandidata = $_POST['categoria'];
-        $nombreVotante = $_POST['user_name'];        
-        $celularVotante = $_POST['user_phone'];
-        $correoVotante = $_POST['user_correo'];
+    if (!$this->dbh){
+        // Manejar el caso donde $this->dbh no está inicializado
+        throw new Exception("La conexión a la base de datos no está inicializada.");
 
-        if (!$this->dbh) {
-            // Manejar el caso donde $this->dbh no está inicializado
-            throw new Exception("La conexión a la base de datos no está inicializada.");
+    }else{
+        $stmt = $this->dbh->prepare("INSERT INTO votos (Nombre_Candidata, Region_Candidata, Categoria, Nombre_Votante, Celular_Votante, Correo_Votante ) 
+        VALUES (:nameCandidata, :regionCandidata, :categoriaCandidata, :nombreVotante, :celularVotante, :correoVotante )");
 
-        }else{
-            $stmt = $this->dbh->prepare("INSERT INTO votos (Nombre_Candidata, Region_Candidata, Categoria, Nombre_Votante, Celular_Votante, Correo_Votante ) 
-            VALUES (:nameCandidata, :regionCandidata, :categoriaCandidata, :nombreVotante, :celularVotante, :correoVotante )");
+        $stmt->bindValue(':nameCandidata', $nameCandidata, PDO::PARAM_STR);
+        $stmt->bindValue(':regionCandidata', $regionCandidata, PDO::PARAM_STR);
+        $stmt->bindValue(':categoriaCandidata', $categoriaCandidata, PDO::PARAM_STR);
+        $stmt->bindValue(':nombreVotante', $nombreVotante, PDO::PARAM_STR);
+        $stmt->bindValue(':celularVotante', $celularVotante, PDO::PARAM_STR);
+        $stmt->bindValue(':correoVotante', $correoVotante, PDO::PARAM_STR);
 
-            $stmt->bindValue(':nameCandidata', $nameCandidata, PDO::PARAM_STR);
-            $stmt->bindValue(':regionCandidata', $regionCandidata, PDO::PARAM_STR);
-            $stmt->bindValue(':categoriaCandidata', $categoriaCandidata, PDO::PARAM_STR);
-            $stmt->bindValue(':nombreVotante', $nombreVotante, PDO::PARAM_STR);
-            $stmt->bindValue(':celularVotante', $celularVotante, PDO::PARAM_STR);
-            $stmt->bindValue(':correoVotante', $correoVotante, PDO::PARAM_STR);
+        $stmt->execute();
+    }     
 
-            $stmt->execute();
-        } 
-        
+    // Mostrar los datos en la consola
+    echo "Datos extraídos del método POST:\n";
+    echo "Nombre Candidata: " . $nameCandidata . "\n";
+    echo "Región Candidata: " . $regionCandidata . "\n";
+    echo "Categoría Candidata: " . $categoriaCandidata . "\n";
+    echo "Nombre Votante: " . $nombreVotante . "\n";
+    echo "Celular Votante: " . $celularVotante . "\n";
+    echo "Correo Votante: " . $correoVotante . "\n";
 
-        // Mostrar los datos en la consola
-        echo "Datos extraídos del método POST:\n";
-        echo "Nombre Candidata: " . $nameCandidata . "\n";
-        echo "Región Candidata: " . $regionCandidata . "\n";
-        echo "Categoría Candidata: " . $categoriaCandidata . "\n";
-        echo "Nombre Votante: " . $nombreVotante . "\n";
-        echo "Celular Votante: " . $celularVotante . "\n";
-        echo "Correo Votante: " . $correoVotante . "\n";
-
-        echo "Datos insertados correctamente.";
+    echo "Datos insertados correctamente.";
 
     }
 }
@@ -89,6 +79,9 @@ try {
 
 } catch (Exception $e) {
     echo $e->getMessage();
+
+}
+?>
 
 }
 
